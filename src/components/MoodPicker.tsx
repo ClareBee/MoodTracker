1c82abef7e7a855e8efd48b531fb945db151bd69
Clare <chblackburne@gmail.com>
2021-12-22 13:45:42 +0000

    Basic moodPicker component

---
+++ src/components/MoodPicker.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { MoodOptionType } from '../types';
+
+const moodOptions: MoodOptionType[] = [
+  { emoji: '🧑‍💻', description: 'studious' },
+  { emoji: '🤔', description: 'pensive' },
+  { emoji: '😊', description: 'happy' },
+  { emoji: '🥳', description: 'celebratory' },
+  { emoji: '😤', description: 'frustrated' },
+];
+
+export const MoodPicker: React.FC = () => {
+  const [selectedMood, setSelectedMood] = useState<MoodOptionType>();
+
+  return (
+    <View style={styles.moodList}>
+      {moodOptions.map(option => (
+        <View key={option.emoji}>
+          <Pressable
+            onPress={() => setSelectedMood(option)}
+            style={[
+              styles.moodItem,
+              option.emoji === selectedMood?.emoji
+                ? styles.selectedMoodItem
+                : undefined,
+            ]}>
+            <Text style={styles.moodText}>{option.emoji}</Text>
+          </Pressable>
+          <Text style={styles.descriptionText}>
+            {selectedMood?.emoji === option.emoji ? option.description : ''}
+          </Text>
+        </View>
+      ))}
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  moodText: {
+    fontSize: 24,
+  },
+  moodList: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+  moodItem: {
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+    marginBottom: 5,
+  },
+  selectedMoodItem: {
+    borderWidth: 2,
+    backgroundColor: '#454C73',
+    borderColor: '#fff',
+  },
+  descriptionText: {
+    color: '#454C73',
+    fontWeight: 'bold',
+    fontSize: 10,
+    textAlign: 'center',
+  },
+});
